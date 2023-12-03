ec1733dd51e75db0bcaafaab565679f20a0b9def
nehemiahcode <codehubby@gmail.com>
2023-12-03 10:59:31 +0100

    second commit

--- components/Login.jsx
+++ components/Login.jsx
@@ -56,7 +56,7 @@ export default function Login() {
               Log in to your account
             </h3>
             <p className="">
-              Don't have an account?{" "}
+              Don&apos;t have an account?{" "}
               <a
                 href="/register"
                 className="font-medium text-indigo-600 hover:text-indigo-500"
